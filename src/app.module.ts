2a3441607660ded220edb53143d68d09f404c643
Abim-Bola <bamgbeluabimbolainfo@gmail.com>
2024-08-18 18:30:00 -0600

    set up e2e tests and set global configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe, MiddlewareConsumer } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
@@ -6,6 +7,7 @@ import { UserModule } from './user/user.module';
 import { ReportModule } from './report/report.module';
 import { User } from './user/user.entity';
 import { Report } from './report/report.entity';
+const cookieSession = require('cookie-session')
 
 @Module({
   imports: [
@@ -19,6 +21,21 @@ import { Report } from './report/report.entity';
     ReportModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+      }),
+    }
+
+  ],
 })
-export class AppModule { }
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) { //set up middleware to run on every incoming request
+    consumer.apply(cookieSession({
+      keys: ['heyabimbola'] //Used to encrypt cookie - Should be stored in env
+    })).forRoutes('*') //global middleware for all routes
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,7 @@ const cookieSession = require('cookie-session')
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieSession({
-    keys: ['heyabimbola'] //Used to encrypt cookie - Should be stored in env
-  }))
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    }),
-  );
+
   await app.listen(3000);
 }
 bootstrap();
